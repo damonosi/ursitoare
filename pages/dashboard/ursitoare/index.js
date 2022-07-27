6e586ae9d3bb3f989afcae1766d5a4a88c5a3ef7
andrei <dam195@yahoo.com>
2022-07-27 11:08:54 +0300

    loading spinner

--- pages/dashboard/ursitoare/index.js
+++ pages/dashboard/ursitoare/index.js
@@ -22,7 +22,6 @@ function reducer(state, action) {
 }
 
 export default function DashboardUrsitoarePage() {
-  const { data: session } = useSession();
   const [{ loading, error, rezervari }, dispatch] = useReducer(reducer, {
     loading: true,
     rezervari: [],
@@ -48,51 +47,57 @@ export default function DashboardUrsitoarePage() {
     <div className={styles.containerDsh}>
       <h1>Rezervari clienti</h1>
       <div className={styles.containerDashboard}>
-        {rezervari.map((eveniment) => (
-          <CasetaEveniment key={eveniment._id}>
-            <h1>{eveniment.numecopil}</h1>
-            <h2>Data Nasterii : {eveniment.datanastere}</h2>
-            <div>
-              <h2>Parinti</h2>
-              <div className={styles.parintiContainer}>
-                <div className={styles.parintiContainerMic}>
-                  <h3>Mama </h3>
-                  <hr />
-                  <h3> {eveniment.mama}</h3>
-                </div>
-                <div className={styles.parintiContainerMic}>
-                  <h3>Tata</h3>
-                  <hr />
-                  <h3> {eveniment.tata}</h3>
+        {loading ? (
+          <div className={styles.loaderContainer}>
+            <div className={styles.loader}></div>
+          </div>
+        ) : (
+          rezervari.map((eveniment) => (
+            <CasetaEveniment key={eveniment._id}>
+              <h1>{eveniment.numecopil}</h1>
+              <h2>Data Nasterii : {eveniment.datanastere}</h2>
+              <div>
+                <h2>Parinti</h2>
+                <div className={styles.parintiContainer}>
+                  <div className={styles.parintiContainerMic}>
+                    <h3>Mama </h3>
+                    <hr />
+                    <h3> {eveniment.mama}</h3>
+                  </div>
+                  <div className={styles.parintiContainerMic}>
+                    <h3>Tata</h3>
+                    <hr />
+                    <h3> {eveniment.tata}</h3>
+                  </div>
                 </div>
               </div>
-            </div>
-            <br />
-            <h2>Frati/ Surori</h2>
+              <br />
+              <h2>Frati/ Surori</h2>
 
-            {eveniment.frati.map((i) => (
-              <div key={eveniment._id} className={styles.fratiContainer}>
-                <h3>{i.nume}</h3>
-                <hr />
-                <h3>{i.varsta} ani</h3>
+              {eveniment.frati.map((i) => (
+                <div key={eveniment._id} className={styles.fratiContainer}>
+                  <h3>{i.nume}</h3>
+                  <hr />
+                  <h3>{i.varsta} ani</h3>
+                </div>
+              ))}
+              <h2> Detalii eveniment</h2>
+              <div className={styles.detaliiEveniment}>
+                <h3>Data evenimentului {eveniment.dataeveniment}</h3>
+                <h3>Ora {eveniment.oraeveniment}.00</h3>
               </div>
-            ))}
-            <h2> Detalii eveniment</h2>
-            <div className={styles.detaliiEveniment}>
-              <h3>Data evenimentului {eveniment.dataeveniment}</h3>
-              <h3>Ora {eveniment.oraeveniment}.00</h3>
-            </div>
-            <button
-              onClick={async (e) => {
-                axios.post(`/api/evenimente/${eveniment._id}/opteaza`);
-                e.currentTarget.disabled = true;
-                toast.success("Ai optat pentru eveniment");
-              }}
-            >
-              Vreau sa merg !
-            </button>
-          </CasetaEveniment>
-        ))}
+              <button
+                onClick={async (e) => {
+                  axios.post(`/api/evenimente/${eveniment._id}/opteaza`);
+                  e.currentTarget.disabled = true;
+                  toast.success("Ai optat pentru eveniment");
+                }}
+              >
+                Vreau sa merg !
+              </button>
+            </CasetaEveniment>
+          ))
+        )}
       </div>{" "}
       <button
         onClick={() => {
