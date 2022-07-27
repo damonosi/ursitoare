b4894f63dcba90932122197c7b330425175f3f72
andrei <dam195@yahoo.com>
2022-07-27 11:52:32 +0300

    loading spinner

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
@@ -9,7 +9,7 @@ import Spinner from "./../../components/spinner/Spinner";
 
 const LoginScreen = () => {
   const { status, data: session } = useSession();
-
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { redirect } = router.query;
 
@@ -26,12 +26,14 @@ const LoginScreen = () => {
   } = useForm();
 
   const submitHandler = async ({ email, password }) => {
+    setLoading(true);
     try {
       const result = await signIn("credentials", {
         redirect: false,
         email,
         password,
       });
+      setLoading(false);
       if (result.error) {
         toast.error(result.error);
       }
@@ -39,63 +41,59 @@ const LoginScreen = () => {
       toast.error(getError(err));
     }
   };
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <div>
-      {status === "loading?" ? (
-        <Spinner />
-      ) : (
-        <form
-          className="mx-auto max-w-screen-md"
-          onSubmit={handleSubmit(submitHandler)}
-        >
-          <h1 className="mb-4 text-xl">Login</h1>
-          <div className="mb-4">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              {...register("email", {
-                required: "Please enter email",
-                pattern: {
-                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                  message: "Please enter valid email",
-                },
-              })}
-              className="w-full"
-              id="email"
-              autoFocus
-            ></input>
-            {errors.email && (
-              <div className="text-red-500">{errors.email.message}</div>
-            )}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              {...register("password", {
-                required: "Please enter password",
-                minLength: {
-                  value: 6,
-                  message: "password is more than 5 chars",
-                },
-              })}
-              className="w-full"
-              id="password"
-              autoFocus
-            ></input>
-            {errors.password && (
-              <div className="text-red-500 ">{errors.password.message}</div>
-            )}
-          </div>
-          <div className="mb-4 ">
-            <button className="primary-button">Login</button>
-          </div>
-          <div className="mb-4 ">
-            Don&apos;t have an account? &nbsp;
-            <Link href="/auth/register">Register</Link>
-          </div>
-        </form>
-      )}
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "Please enter email",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: "Please enter valid email",
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "Please enter password",
+              minLength: { value: 6, message: "password is more than 5 chars" },
+            })}
+            className="w-full"
+            id="password"
+            autoFocus
+          ></input>
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="mb-4 ">
+          <button className="primary-button">Login</button>
+        </div>
+        <div className="mb-4 ">
+          Don&apos;t have an account? &nbsp;
+          <Link href="/auth/register">Register</Link>
+        </div>
+      </form>
     </div>
   );
 };
