6e9b3ea56974e3ccd502b963d689662022163a6f
andrei <dam195@yahoo.com>
2022-08-17 10:39:19 +0300

    am mutat dashboard in /user

--- pages/api/rezervari/rezervarile-mele.js
+++ pages/api/rezervari/rezervarile-mele.js
@@ -1,10 +1,11 @@
 import { User } from "../../../models/User";
 
 const handler = async (req, res) => {
-  const userId = req.headers._id;
+  const userId = await req.headers._id;
   const userUp = await User.findById(userId);
 
-  const rezFacute = userUp.rezervarilemele;
+  let rezFacute = userUp.rezervarilemele;
+
   res.send(rezFacute);
 };
 
