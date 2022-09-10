7363903c9a45ec5d5dc9df7e0cdfb52b26cf6419
andrei <dam195@yahoo.com>
2022-09-10 14:06:51 +0300

    admin menu styles close open

--- components/header/admin/index.jsx
+++ components/header/admin/index.jsx
@@ -1,94 +1,124 @@
 import Link from "next/link";
 
 import styles from "./Menu.module.scss";
-
+import { useState } from "react";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 const AdminMenu = () => {
+  const [mobileMenu, setCloseMobileMenu] = useState(false);
   return (
-    <div className={styles.adminMenuContainer}>
-      <h1>Panou de Comanda</h1>
+    <>
+      {!mobileMenu ? (
+        <div className={styles.openArrow}>
+          <div className={styles.tooltip}>
+            <AiOutlineArrowRight
+              size={"2em"}
+              onClick={() => setCloseMobileMenu(!mobileMenu)}
+            />
+            <span className={styles.tooltiptext}>Deschide Meniu</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
 
-      <hr />
-      <ul>
-        <li>
-          <Link href="/dashboard/admin/creati-un-eveniment">
-            <a>
-              <button className="explore">
-                CREATI UN EVENIMENT<span className={styles.iconRight}></span>
-                <span
-                  className={`${styles.iconRight} ${styles.after}`}
-                  data-before="CREATI"
-                ></span>
-              </button>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/confirmati-evenimentul">
-            <a>
-              <button className="explore">
-                Confirmati Evenimentul<span className={styles.iconRight}></span>
-                <span
-                  className={`${styles.iconRight} ${styles.after}`}
-                  data-before="Confirma"
-                ></span>
-              </button>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/evenimente-acceptate">
-            <a>
-              <button className="explore">
-                EVENIMENTE ACCEPTATE<span className={styles.iconRight}></span>
-                <span
-                  className={`${styles.iconRight} ${styles.after}`}
-                  data-before="EVENIMENTE"
-                ></span>
-              </button>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/adauga-ursitoare">
-            <a>
-              <button className="explore">
-                ADAUGA URSITOARE<span className={styles.iconRight}></span>
-                <span
-                  className={`${styles.iconRight} ${styles.after}`}
-                  data-before="URSITOARE"
-                ></span>
-              </button>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/program-azi">
-            <a>
-              <button className="explore">
-                PROGRAM ASTAZI<span className={styles.iconRight}></span>
-                <span
-                  className={`${styles.iconRight} ${styles.after}`}
-                  data-before="AZI"
-                ></span>
-              </button>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard/admin/cereri-de-rezervare">
-            <a>
-              <button className="explore">
-                PROGRAM SAPTAMANA ASTA<span className={styles.iconRight}></span>
-                <span
-                  className={`${styles.iconRight} ${styles.after}`}
-                  data-before="SAPTAMANA ASTA"
-                ></span>
-              </button>
-            </a>
-          </Link>
-        </li>
-      </ul>
-    </div>
+      <div
+        className={
+          mobileMenu ? styles.adminMenuContainer : styles.closedAdminMenu
+        }
+      >
+        <h1>Panou de Comanda</h1>
+
+        <hr />
+        <div className={styles.closeArrow}>
+          <AiOutlineArrowLeft
+            size={"2em"}
+            onClick={() => setCloseMobileMenu(!mobileMenu)}
+          />
+        </div>
+        <ul>
+          <li>
+            <Link href="/dashboard/admin/creati-un-eveniment">
+              <a>
+                <button className="explore">
+                  CREATI UN EVENIMENT<span className={styles.iconRight}></span>
+                  <span
+                    className={`${styles.iconRight} ${styles.after}`}
+                    data-before="CREATI"
+                  ></span>
+                </button>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/admin/confirmati-evenimentul">
+              <a>
+                <button className="explore">
+                  Confirmati Evenimentul
+                  <span className={styles.iconRight}></span>
+                  <span
+                    className={`${styles.iconRight} ${styles.after}`}
+                    data-before="Confirma"
+                  ></span>
+                </button>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/admin/evenimente-acceptate">
+              <a>
+                <button className="explore">
+                  EVENIMENTE ACCEPTATE<span className={styles.iconRight}></span>
+                  <span
+                    className={`${styles.iconRight} ${styles.after}`}
+                    data-before="EVENIMENTE"
+                  ></span>
+                </button>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/admin/adauga-ursitoare">
+              <a>
+                <button className="explore">
+                  ADAUGA URSITOARE<span className={styles.iconRight}></span>
+                  <span
+                    className={`${styles.iconRight} ${styles.after}`}
+                    data-before="URSITOARE"
+                  ></span>
+                </button>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/admin/program-azi">
+              <a>
+                <button className="explore">
+                  PROGRAM ASTAZI<span className={styles.iconRight}></span>
+                  <span
+                    className={`${styles.iconRight} ${styles.after}`}
+                    data-before="AZI"
+                  ></span>
+                </button>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/admin/cereri-de-rezervare">
+              <a>
+                <button className="explore">
+                  PROGRAM SAPTAMANA ASTA
+                  <span className={styles.iconRight}></span>
+                  <span
+                    className={`${styles.iconRight} ${styles.after}`}
+                    data-before="SAPTAMANA ASTA"
+                  ></span>
+                </button>
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
