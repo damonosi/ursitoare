24b5abd73d5c79873cdf86152a10f5310fa7c5e7
andrei <dam195@yahoo.com>
2022-09-21 13:43:24 +0300

    imputinat date api ursitoare

--- pages/api/ursitoare/index.js
+++ pages/api/ursitoare/index.js
@@ -5,8 +5,13 @@ const handler = async (req, res) => {
   await db.connect();
   const Ursaitoare = await User.find({ isursitoare: true });
 
+  let UrsMinim = new Array();
+  Ursaitoare.map((ursitoare) => {
+    UrsMinim.push({ name: ursitoare.name, _id: ursitoare._id });
+  });
+  console.log(UrsMinim);
   await db.disconnect();
-  res.send(Ursaitoare);
+  res.send(UrsMinim);
 };
 
 export default handler;
